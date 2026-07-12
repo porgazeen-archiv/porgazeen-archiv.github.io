c659f06e48e1893c141dc7fde772358cd72f1778
knihovnik2 <lukas.stanovsky77@gmail.com>
2026-07-12 18:15:11 +0200

    pridat cislo 92

--- ziskani_titulni_strany_z_pdf.py
+++ ziskani_titulni_strany_z_pdf.py
@@ -2,11 +2,13 @@ from PIL import Image
 
 from pdf2image import convert_from_path
 
-images = convert_from_path("/home/lukas/Python/Porgazeen/casopisy/91.pdf", first_page=1, last_page=1)
-images[0].save("/home/lukas/Python/Porgazeen/titulni_strany/91.jpg", "JPEG")
+cislo = "92"
 
-img = Image.open("/home/lukas/Python/Porgazeen/titulni_strany/91.jpg")
+images = convert_from_path(f"/home/lukas/Python/Porgazeen/casopisy/{cislo}.pdf", first_page=1, last_page=1)
+images[0].save(f"/home/lukas/Python/Porgazeen/titulni_strany/{cislo}.jpg", "JPEG")
+
+img = Image.open(f"/home/lukas/Python/Porgazeen/titulni_strany/{cislo}.jpg")
 
 resized = img.resize((198, 280), Image.LANCZOS)
 
-resized.save("/home/lukas/Python/Porgazeen/titulni_strany/91.jpg")
+resized.save(f"/home/lukas/Python/Porgazeen/titulni_strany/{cislo}.jpg")
